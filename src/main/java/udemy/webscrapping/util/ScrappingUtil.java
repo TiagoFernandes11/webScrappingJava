7b97f504e452b8120fd2ecbd2f3356f3678e8930
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-04-27 11:23:04 -0300

    enum created

---
+++ src/main/java/udemy/webscrapping/util/ScrappingUtil.java
@@ -0,0 +1,10 @@
+package udemy.webscrapping.util;
+
+public class ScrappingUtil {
+
+    private static final String BASE_SEARCH_URL_GOOGLE = "https://www.google.com.br/search?q=";
+    private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
+    public static void main(String[] args) {
+
+    }
+}
